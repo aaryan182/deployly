0dfbc7eddc675f3f4488e454b35474adc19b2979
aaryan <aaryanbajaj385@gmail.com>
2024-11-23 12:39:19 +0530

    Feat: coded deploying code service

---
+++ deployly-deploy-service/src/index.ts
@@ -0,0 +1,23 @@
+import { createClient, commandOptions } from "redis";
+import { copyFinalDist, downloadS3Folder } from "./cloudflareDownload";
+import { buildProject } from "./runbuild";
+const subscriber = createClient();
+subscriber.connect();
+
+async function main() {
+  while (1) {
+    const response = await subscriber.brPop(
+      commandOptions({ isolated: true }),
+      "build-queue",
+      0
+    );
+    console.log(response);
+    // @ts-ignore
+    const id = response.element;
+    await downloadS3Folder(`/output/${id}`);
+    await buildProject(id);
+    copyFinalDist(id)
+  }
+}
+
+main();
--- src/cloudflareUpload.ts
+++ src/cloudflareUpload.ts
@@ -1,11 +1,13 @@
 import { S3 } from "aws-sdk";
 import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const s3 = new S3({
-  accessKeyId: "d22d0abd22fc5b09887b936fe7cf687e",
-  secretAccessKey:
-    "bbe6d8b28b414a0cbfd98ed51b317d9c51cfadc83d551f52018a3a2b5492332b",
-  endpoint: "https://198a5de935e581b753ddad2f2c076991.r2.cloudflarestorage.com",
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  endpoint: process.env.endpoint,
 });
 
 export const uploadFile = async (fileKey: string, localFilePath: string) => {
@@ -14,8 +16,8 @@ export const uploadFile = async (fileKey: string, localFilePath: string) => {
     const response = await s3
       .upload({
         Body: fileContent,
-        Bucket: "deployly", 
-        Key: fileKey, 
+        Bucket: "deployly",
+        Key: fileKey,
       })
       .promise();
     console.log(`Uploaded: ${fileKey}`);
